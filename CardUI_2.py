69e0d1f2a64410b05765bf496c80c5a6bc87a63d
LaFlame1228 <chen4002812@gmail.com>
2020-11-17 17:43:14 +0800

    1117

--- CardUI_2.py
+++ CardUI_2.py
@@ -3,21 +3,60 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.core.text import LabelBase
 LabelBase.register(name='Font_Hanzi',fn_regular="mysh.ttf")
 
-class CardUIWindow(Widget):
+class CardNumber(GridLayout):
+    def __init__(self, **kwargs):
+        super(CardNumber,self).__init__(**kwargs)
+        self.init_card = NumericProperty(0)
+        self.sent_card = NumericProperty(0)
+        self.recycled_card = NumericProperty(0)
+        self.remaining_card = NumericProperty(0)
+        self.card_counter = 1
+        self.recycle_flag = 0
+        self.send_flag = 0
+
+    def update(self):
+        if self.recycle_flag == 1:
+            self.remaining_card.text = str(int(self.init_card.text) - self.card_counter)
+            self.recycled_card.text = str(int(self.recycled_card.text) + 1)
+        if self.send_flag == 1:
+            self.remaining_card.text = str(int(self.init_card.text) - self.card_counter)
+            self.sent_card.text = str(int(self.sent_card.text) + 1)
+
+    def btn_recycle(self):
+        self.recycle_flag = 1
+
+    def btn_send(self):
+        self.send_flag = 1
+
+    def release(self):
+        self.update()
+        self.card_counter += 1
+        self.recycle_flag = 0
+        self.send_flag = 0
+
+
+class PlateNumber(GridLayout):
+    pass
+
+class LogText(BoxLayout):
+    pass
+
+class StatusBar(GridLayout):
+    pass
+
+class CardUIWindow(BoxLayout):
     # def __init__(self, **kwargs):
     #     super().__init__(**kwargs)
         pass
 
-class CardNumber(Widget):
-    pass
-
-
 class CardUI2App(App):
 
     def build(self):
+
         return CardUIWindow()
 
 if __name__ == '__main__':
--- gametest.py
+++ gametest.py
@@ -1,52 +1,20 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.lang import Builder
-from kivy.uix.bubble import Bubble
-
-Builder.load_string('''
-<cut_copy_paste>
-    size_hint: (None, None)
-    size: (160, 120)
-    pos_hint: {'center_x': .5, 'y': .6}
-    BubbleButton:
-        text: 'Cut'
-    BubbleButton:
-        text: 'Copy'
-    BubbleButton:
-        text: 'Paste'
-''')
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
 
-class cut_copy_paste(Bubble):
-    pass
-
-
-class BubbleShowcase(FloatLayout):
-
-    def __init__(self, **kwargs):
-        super(BubbleShowcase, self).__init__(**kwargs)
-        self.but_bubble = Button(text='Press to show bubble')
-        self.but_bubble.bind(on_release=self.show_bubble)
-        self.add_widget(self.but_bubble)
-
-    def show_bubble(self, *l):
-        if not hasattr(self, 'bubb'):
-            self.bubb = bubb = cut_copy_paste()
-            self.add_widget(bubb)
-        else:
-            values = ('left_top', 'left_mid', 'left_bottom', 'top_left',
-                'top_mid', 'top_right', 'right_top', 'right_mid',
-                'right_bottom', 'bottom_left', 'bottom_mid', 'bottom_right')
-            index = values.index(self.bubb.arrow_pos)
-            self.bubb.arrow_pos = values[(index + 1) % len(values)]
-
-
-class TestBubbleApp(App):
-
+class MyApp(App):
     def build(self):
-        return BubbleShowcase()
+        layout = BoxLayout(orientation='vertical')
+        answer = TextInput(multiline=False, text="", size_hint=(1, 0.5))
+        ansLen = Label(bold=True, halign="center", text="", size_hint=(1, 0.5))
+
+        answer.bind(text=lambda instance, text: setattr(ansLen, "text", str(len(text))))
+        layout.add_widget(answer)
+        layout.add_widget(ansLen)
+        return layout
 
 
 if __name__ == '__main__':
-    TestBubbleApp().run()
+    MyApp().run()
